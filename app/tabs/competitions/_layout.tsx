92ec70f3553be4d33c704a532b4e1be6c2e8a740
Ben Wei <benwei@Bens-Air.lan>
2024-09-22 12:34:06 -0700

    function naming changes

--- app/tabs/competitions/_layout.tsx
+++ app/tabs/competitions/_layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Stack } from 'expo-router';
 
-export default function HomeStack() {
+export default function CompetitionStack() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: 'Competitions List', headerShown: false }} />
