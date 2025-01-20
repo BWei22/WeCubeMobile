09b520f141411c7c363f88a612042da5e4d21bb2
Ben Wei <benwei@Bens-MacBook-Air.local>
2025-01-20 12:45:28 -0500

    routing issues resolved

--- app/tabs/competitions/_layout.tsx
+++ app/tabs/competitions/_layout.tsx
@@ -4,8 +4,29 @@ import { Stack } from 'expo-router';
 export default function CompetitionStack() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Competitions List', headerShown: false }} />
-      <Stack.Screen name="[competitionId]" options={{ title: 'This Competition', headerShown: false }} />
+      {/* Competitions List */}
+      <Stack.Screen
+        name="index"
+        options={{ title: '', headerShown: false }}
+      />
+
+      {/* Specific Competition */}
+      <Stack.Screen
+        name="[competitionId]/index"
+        options={{ title: '', headerShown: true }}
+      />
+
+      {/* Create New Listing */}
+      <Stack.Screen
+        name="[competitionId]/create-listing"
+        options={{ title: '', headerShown: true }}
+      />
+
+      {/* Specific Listing */}
+      <Stack.Screen
+        name="[competitionId]/[listingId]/index"
+        options={{ title: '', headerShown: true }}
+      />
     </Stack>
   );
 }
--- app/tabs/messages/_layout.tsx
+++ app/tabs/messages/_layout.tsx
@@ -4,8 +4,8 @@ import { Stack } from 'expo-router';
 export default function MessagesStack() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Messages', headerShown: false }} />
-      <Stack.Screen name="[messageId]" options={{ title: 'This Conversation', headerShown: false }} />
+      <Stack.Screen name="index" options={{ title: '', headerShown: false }} />
+      <Stack.Screen name="[messageId]/index" options={{ title: '', headerShown: true }} />
     </Stack>
   );
 }
